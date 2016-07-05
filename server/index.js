c3b35ce2e078aa0f41339fd606e37c881e0901f2
Donovan Hiland <donovan.hiland@gmail.com>
2016-07-05 10:18:00 -0600

    more skele

--- server/index.js
+++ server/index.js
@@ -2,11 +2,33 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import config from './config.js';
+import passport from 'passport';
+let GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(__dirname + '/../public'));
+
+passport.use(new GoogleStrategy({
+    clientID: config.google.GOOGLE_CLIENT_ID,
+    clientSecret: config.google.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+  }
+));
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: [ 'https://www.googleapis.com/auth/plus.me', 'https://www.googleapis.com/auth/calendar'] }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  function(req, res) {
+    console.log('eyy baby');
+  });
 
 app.listen(config.PORT, () => {
   console.log('listening on port: ' + config.PORT);
